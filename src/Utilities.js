86832d98a846513a8f2cd911d485ba0b0c711f5f
Campbell <csinghas@ucsd.edu>
2024-02-20 01:28:10 -0800

    calling new get_csrf api endpoint on app load

--- src/Utilities.js
+++ src/Utilities.js
@@ -32,7 +32,6 @@ export function getCookie(name) {      //Taken from Django docs
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            console.log(cookie);
 
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
