e3c0144a4d4e8eadca9cd279efb4443df0d947cf
singhacinnamon <csinghas@ucsd.edu>
2024-08-02 08:19:35 -0700

    Login updated with validation/err messages as well as working now!

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { getCookie } from "./Utilities"
+import { getCookie, validUserPass } from "./Utilities"
 
 function Login({ setGlobUser  }) {
     const apiUrl = process.env.REACT_APP_API_URL;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [validMsg, setValidMsg] = useState([]);
     const navigate = useNavigate();
-    const [failed, setFailed] = useState(false);
+    const [taken, setTaken] = useState(false);
 
     const handleLogin = async () => {
         const data = {
             username: username,
             password: password,
         };
+        let validMsgVar = validUserPass(username, password)
+        setValidMsg(validMsgVar);
+        if(validMsgVar.length > 0){
+            return;
+        }
         const csrf = getCookie('csrftoken');
     
         const response = await fetch(`${apiUrl}/ling_login/`, {
@@ -27,11 +33,11 @@ function Login({ setGlobUser  }) {
         });
 
         if (response.ok) {
-            setFailed(false);
+            setTaken(false);
             setGlobUser(username)
             navigate(-1);
         } else {
-            setFailed(true);
+            setTaken(true);
         }
     }
     
@@ -52,7 +58,8 @@ function Login({ setGlobUser  }) {
                         <br/>
                         <input type="password" id="password" name="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    { failed ? <><label className="error-text">Username or password was incorrect. Please try again.</label><br/></>: <></>}
+                    { taken ? <><label className="error-text">Username or password was incorrect. Please try again.</label><br/></>: <></>}
+                    { validMsg.map((msg) => <><label className="error-text">{ msg }</label><br></br></>) }
                     <button type="button" className="btn btn-db" onClick={ handleLogin }><h5 className="w5">Log In</h5></button>
                 </form>
             </div>
